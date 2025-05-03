b68d3e84851351f1cfd7b53316550f443bea225f
abdallahsoliman00 <abdallahsoliman03@gmail.com>
2025-05-03 20:31:51 +0100

    disabled camera roll for 3D plots

--- Graph Plotting/3D_plots.py
+++ Graph Plotting/3D_plots.py
@@ -2,20 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.arange(-5, 5, 0.01)   # Define x domain
-y = np.arange(-5, 5, 0.01)   # Define y domain
+x = np.linspace(-5, 5, 200)   # Define x domain
+y = np.linspace(-5, 5, 200)  # Define y domain
 X, Y = np.meshgrid(x, y)
 
-# Define the function Z in terms of X and Y
-# Z = X**2 * Y**2 + X**4
-# Z = np.arctan(9*X/Y)
-# Z = 4*(X**2/49 + Y**2/49)
-# Z = X*np.sin(0.15*Y) + Y*np.sin(0.15*X)
+"""
+# Define the function Z in terms of X and Y:
+# Examples:
+Z = X**2 * Y**2 + X**4
+Z = np.arctan(9*X/Y)
+Z = 4*(X**2/49 + Y**2/49)
+Z = X*np.sin(0.15*Y) + Y*np.sin(0.15*X)
 Z = -6*X*Y - 3*Y**2 - X**2
+"""
+Z = np.arctan(9*X/Y)
+
 
 # Initialize plot
 plt.style.use('dark_background')
-ax = plt.axes(projection="3d")
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Disable camera rolling
+def disable_roll(event):
+    azim, elev = ax.azim, ax.elev
+    ax.view_init(elev=elev, azim=azim, roll=0)
+
+# Connect to motion_notify_event
+fig.canvas.mpl_connect('motion_notify_event', disable_roll)
 
 # Plot and label 3D surface
 ax.set(xlabel='x', ylabel='y', zlabel='z')
